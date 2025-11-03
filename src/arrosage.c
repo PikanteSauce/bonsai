4b42e5493e623e95da855cd9093cad3c6b45540c
PikanteSauce <hugomga@protonmail.com>
2025-11-03 13:38:28 +0100

    Update arrosage.c

--- src/arrosage.c
+++ src/arrosage.c
@@ -4,7 +4,13 @@
   1. Implémenter une horloge pour n'arroser qu'une ou deux fois par jour
   2. Si humidité < Seuil bas => arroser la plante
     a. Mettre en place un hystérésis ou bien une quantité d'eau prédéfinie
-
+  3. Implémentation mode automatique
+      Arrosage des plantes par lecture humidité et décision système
+      Lecture des capterus toutes les 1 minutes
+  4. Implémentation mode manuel 
+      Activer arrosage 
+      Visualisation niveau d'humidité
+    
 
 
 
