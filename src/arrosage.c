9527a52f16057e1612c3a5f01e6c817340f9b7d3
PikanteSauce <hugomga@protonmail.com>
2025-11-03 09:31:52 +0100

    Update arrosage.c

--- src/arrosage.c
+++ src/arrosage.c
@@ -1,2 +1,27 @@
 /************** Logique d'arrosage pour le bonsai/la plante  **************/
 
+/* 
+  1. Implémenter une horloge pour n'arroser qu'une ou deux fois par jour
+  2. Si humidité < Seuil bas => arroser la plante
+    a. Mettre en place un hystérésis ou bien une quantité d'eau prédéfinie
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
